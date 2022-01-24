fc95406d88479b52340968e58b6ab0d7e9e6729a
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-25 00:13:35 +0200

    Finish nav styling

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,18 +1,34 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 
-const NavBar = () => (
-  <nav>
-    <h1>space</h1>
-    <ul>
-      <li>
-        <NavLink to="/" />
-        Rockets
-      </li>
-      <li><NavLink to="missions">Missions</NavLink></li>
-      <li><NavLink to="profile">Profile</NavLink></li>
-    </ul>
-  </nav>
-);
+const NavBar = () => {
+  const location = useLocation();
+  console.log(location.pathname);
+
+  return (
+    <nav className="flex justify-between px-6 py-3 bg-white items-center border-b border-b-gray-300">
+      <h1 className="font-bold text-2xl">Space Travelers&apos; Hub</h1>
+      <ul className="flex gap-3 font-medium">
+        <li className={location.pathname === '/' && 'text-blue-500'}>
+          <NavLink to="/">Rockets</NavLink>
+        </li>
+        <li
+          className={`border-l-black border-l-2 pl-3 ${
+            location.pathname === '/missions' && 'text-blue-500'
+          }`}
+        >
+          <NavLink to="missions">Missions</NavLink>
+        </li>
+        <li
+          className={`border-l-black border-l-2 pl-3 ${
+            location.pathname === '/profile' && 'text-blue-500'
+          }`}
+        >
+          <NavLink to="profile">Profile</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
+};
 
 export default NavBar;
