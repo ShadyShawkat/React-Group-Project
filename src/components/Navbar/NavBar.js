82e8bdd71b57331fdbd21a7fc30154f0d4f21da2
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-25 19:46:11 +0200

    Make navbar fixed

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavLink } from 'react-router-dom';
 
 const NavBar = () => (
-  <nav className="flex justify-between px-6 py-3 bg-white items-center border-b border-b-gray-300">
+  <nav className="flex justify-between px-6 py-3 bg-white items-center border-b border-b-gray-300 fixed top-0 w-screen">
     <h1 className="font-bold text-2xl">Space Travelers&apos; Hub</h1>
     <ul className="flex gap-3 font-medium">
       <li>
