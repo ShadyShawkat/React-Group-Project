e0142f607897e9da334dc90b701f5e7d209faf3c
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-25 18:57:32 +0200

    Render rocket list array

---
+++ src/components/Rockets/Rockets.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+import RocketItem from '../RocketItem/RocketItem';
+
+const Rockets = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
+  console.log(rockets);
+  return (
+    <ul>
+      {rockets.map((rocket) => (
+        <li key={rocket.id}>
+          <RocketItem rocket={rocket} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default Rockets;
