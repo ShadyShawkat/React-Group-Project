1437f1f715e20f91e417da14f1cdfb92be2ccf18
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-26 19:54:10 +0200

    Fix rockets display in profile

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -11,18 +11,25 @@ const Profile = () => {
       </div>
       <div className="basis-1/3">
         <h3 className="text-xl font-bold">My Rockets</h3>
-        <ul className="flex flex-col mt-3 border-2 divide-y-2 divide-slate-300 rounded-md border-slate-400">
-          {rockets.map((rocket) => (
-            <li key={rocket.id} className="p-2 flex gap-5 items-center">
-              <img
-                src={rocket.imageSrc}
-                className="w-8 h-8 rounded-sm"
-                alt="rocket"
-              />
-              <p>{rocket.name}</p>
-            </li>
-          ))}
-        </ul>
+        {rockets.length === 0 && (
+          <div className="text-3xl text-gray-400 font-bold select-none mt-3">
+            No rockets reserved
+          </div>
+        )}
+        {rockets.length > 0 && (
+          <ul className="flex flex-col mt-3 border-2 divide-y-2 divide-slate-300 rounded-md border-slate-400">
+            {rockets.map((rocket) => (
+              <li key={rocket.id} className="p-2 flex gap-5 items-center">
+                <img
+                  src={rocket.imageSrc}
+                  className="w-8 h-8 rounded-sm"
+                  alt="rocket"
+                />
+                <p>{rocket.name}</p>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
