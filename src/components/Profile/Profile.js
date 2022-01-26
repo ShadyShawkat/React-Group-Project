6da0304b23c0e605d7ec334123b315219432ca04
iwillteixeira <iwill2005@gmail.com>
2022-01-26 15:50:53 -0300

    Add mission view in profile section

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,13 +1,32 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+
 // import PropTypes from 'prop-types';
 
 const Profile = () => {
   const rockets = useSelector((state) => state.rockets.rockets.filter((rocket) => rocket.reserved));
+  const missions = useSelector((state) => {
+    const { missions } = state.missions;
+    return missions.filter((missions) => missions.reserved);
+  });
   return (
     <div className="flex justify-center gap-6">
       <div className="basis-1/3">
         <h3 className="text-xl font-bold">My Missions</h3>
+        {missions.length === 0 && (
+          <div className="text-3xl text-gray-400 font-bold select-none mt-3">
+            Not member yet
+          </div>
+        )}
+        {missions.length > 0 && (
+          <ul className="flex flex-col mt-3 border-2 divide-y-2 divide-slate-300 rounded-md border-slate-400">
+            {missions.map((missions) => (
+              <li key={missions.mission_id} className="p-2 flex gap-5 items-center">
+                <p>{missions.mission_name}</p>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
       <div className="basis-1/3">
         <h3 className="text-xl font-bold">My Rockets</h3>
