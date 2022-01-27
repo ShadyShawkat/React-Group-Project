80827850ed1c148fb1e42db605e25f57a6ca1bed
ShadyShawkat <shadyshawkat91@gmail.com>
2022-01-27 22:18:04 +0200

    Add app logo

--- src/components/Navbar/NavBar.js
+++ src/components/Navbar/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import logo from '../../app-logo.png';
 
 const NavBar = () => (
   <nav className="flex px-6 py-3 bg-white items-center border-b border-b-gray-300 fixed top-0 w-screen z-10">
-    <h1 className="font-bold text-2xl hidden sm:block">
-      Space Travelers&apos; Hub
+    <h1 className="font-bold text-2xl flex items-center gap-3">
+      <img src={logo} alt="logo" className="w-12 h-12" />
+      <span className=" hidden md:block">Space Travelers&apos; Hub</span>
     </h1>
     <ul className="flex gap-3 font-medium justify-end flex-1">
       <li>
