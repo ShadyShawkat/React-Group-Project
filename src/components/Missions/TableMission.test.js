e854d4d68b0ddc2c68e2b1179f3f95613706d24b
iwillteixeira <iwill2005@gmail.com>
2022-01-27 13:47:55 -0300

    Fix linters on tests

--- src/components/Missions/TableMission.test.js
+++ src/components/Missions/TableMission.test.js
@@ -1,39 +1,35 @@
 import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import TableMission from './TableMission';
-import store from '../../redux/store'
-
-
-
+import store from '../../redux/store';
 
 describe('Test TableMission render', () => {
   test('Test TableMission', () => {
     render(
-        <Provider store={store}>
-            <TableMission />
-        </Provider>
+      <Provider store={store}>
+        <TableMission />
+      </Provider>,
     );
     expect(screen.getByText('Mission')).toBeInTheDocument();
   });
 
   test('Test TableMission', () => {
     render(
-        <Provider store={store}>
-            <TableMission />
-        </Provider>
+      <Provider store={store}>
+        <TableMission />
+      </Provider>,
     );
     expect(screen.getByText('Description')).toBeInTheDocument();
   });
 
   test('Test TableMission', () => {
     render(
-        <Provider store={store}>
-            <TableMission />
-        </Provider>
+      <Provider store={store}>
+        <TableMission />
+      </Provider>,
     );
     expect(screen.getByText('Status')).toBeInTheDocument();
   });
-  
 });
